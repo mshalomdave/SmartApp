74173aafdf5dd078e42969bc7486cf5619dc80ce
Shalom Dave <mshalomdave@gmail.com>
2024-03-29 16:26:30 +0300

    Android Programming First Lesson update - Some small helpful information

--- app/src/main/java/com/sitm/smartapp/MainActivity.java
+++ app/src/main/java/com/sitm/smartapp/MainActivity.java
@@ -16,20 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize our views
         textView1=findViewById(R.id.textView1);
         editText1=findViewById(R.id.editText1);
         Button btnSubmit =findViewById(R.id.submitBtn);
 
+        //Set an onclick listener for the submit button
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Call function submit to change our textView
                 submit();
             }
         });
     }
 
+    //This method is for the purpose of changing our textView's text
     public void submit(){
+        //Get the text from our editText ad store it in name variable
         String name=editText1.getText().toString();
+        //View the text in the textview
         textView1.setText("Hello "+name);
     }
 
